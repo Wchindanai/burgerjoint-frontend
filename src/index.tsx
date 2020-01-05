b0f2396af67ffa4cb1616971e5860ff2de0204b7
Chindanai Wasanachin <chindanai.wasanachin@accenture.com>
2020-01-06 00:59:11 +0700

    delete unuse lib

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import './index.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import * as serviceWorker from './serviceWorker'
-
-const client = new ApolloClient({
-  uri: 'https://asia-east2-theburgerjoint-1562173328860.cloudfunctions.net/app/graphql',
-})
+import './index.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Router>
-      <Switch>
-        <Route component={App}/>
-      </Switch>
-    </Router>
-  </ApolloProvider>,
-  document.getElementById('root'),
-)
+  <Router>
+    <Switch>
+      <Route component={App} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
